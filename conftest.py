0088efc719a4611cca4a4be7d93af1b50391ce4c
gampadich <gampadich@gmail.com>
2026-08-09 14:58:14 +0300

    Correct test

--- conftest.py
+++ conftest.py
@@ -12,12 +12,17 @@ def pytest_runtest_makereport(item, call):
     setattr(item, "rep_" + report.when, report)
 
 @pytest.fixture(autouse=True)
-def attach_screenshot_on_failure(request, page):
+def attach_screenshot_on_failure(request):
     yield
     if hasattr(request.node, "rep_call") and request.node.rep_call.failed:
-        screenshot = page.screenshot(full_page=True)
-        allure.attach(
-            screenshot,
-            name="screenshot_on_failure",
-            attachment_type=allure.attachment_type.PNG
-        )
+        page = request.node.funcargs.get("page")
+        if page and not page.is_closed():
+            try:
+                screenshot = page.screenshot(full_page=True)
+                allure.attach(
+                    screenshot,
+                    name="screenshot_on_failure",
+                    attachment_type=allure.attachment_type.PNG
+                )
+            except Exception as e:
+                print(f"Failed to take screenshot: {e}")
